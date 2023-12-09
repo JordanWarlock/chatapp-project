2ed42305266380bad541b245d99183d3ed689ce1
JordanWarlock <farari14567@gmail.com>
2023-12-09 12:55:06 +0500

    remvome the node_modules

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,23 @@
-const express= require("express");
-const { chats }= require("./Data/data");
-const dotenv=require("dotenv");
+const express = require("express");
+const { chats } = require("./Data/data");
+const dotenv = require("dotenv");
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("Api is running");
+app.get("/", (req, res) => {
+  res.send("Api is running");
 });
 
-app.get('/api/chat',(req,res)=>{
-    res.send(chats);
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
 });
 
-app.get('/api/chat/:id',(req,res)=>{
-   // console.log(req.params.id);
-   const singleChat = chats.find((c)=> c._id===req.params.id);
-   res.send(singleChat);
-})
+app.get("/api/chat/:id", (req, res) => {
+  //console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
 
-const PORT=process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(5000, console.log(`Server Started on PORT ${PORT}`));

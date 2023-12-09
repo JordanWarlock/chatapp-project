47b2be5e33c2843a2f9e3159230eb59f864d3326
gulshwrkhan <147302074+gulshwrkhan@users.noreply.github.com>
2023-12-09 22:56:47 +0500

    Made the home page for login and signup

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,10 +1,55 @@
 import React from 'react'
+import {
+  Container,
+  Box,
+  Text,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs, 
+} from '@chakra-ui/react' 
+import Login from '../components/Authentication/Login';
+import Sign from '../components/Authentication/Sign';
+
 
 const Homepage = () => {
   return (
-    <div>
-      Home
-    </div>
+    <Container maxW='xl' centerContent>
+      <Box
+      d="flex"
+      justifyContent={"center"}
+      p={3}
+      bg={"white"}
+      w="100%"
+      m="40px 0 15px 0"
+      borderRadius="lg"
+      borderWidth="1px">
+      <Text fontSize="4xl" textAlign={"center"} fontFamily="work sans" color="black">Talk-A-Tive</Text>
+      </Box>
+      <Box
+      bg="white"
+      w="100%"
+      p={4}
+      borderRadius={"lg"}
+      borderWidth={"1px"}
+      textColor={"black"}>
+        <Tabs variant='soft-rounded'>
+  <TabList mb="lem">
+    <Tab width={"50%"}>Login</Tab>
+    <Tab width={"50%"}>Sign Up</Tab>
+  </TabList>
+  <TabPanels>
+    <TabPanel>
+      <Login/>
+    </TabPanel>
+    <TabPanel>
+      <Sign/>
+    </TabPanel>
+  </TabPanels>
+</Tabs>
+      </Box>
+    </Container>
   );
 };
 
